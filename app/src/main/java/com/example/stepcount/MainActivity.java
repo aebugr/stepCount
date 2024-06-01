a13c73560772eda60182c5746ec1b255b9969025
Sneja <iliuvv@mail.ru>
2024-06-01 12:41:47 +0500

    Разработанная практическая работа на оценку 3.  Бугрина

--- app/src/main/java/com/example/stepcount/MainActivity.java
+++ app/src/main/java/com/example/stepcount/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.stepcount;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
@@ -53,5 +54,31 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             Button button = findViewById(R.id.button);
             button.setText("ПАУЗА");
         }
+    }
+    public void onSensorChanged(SensorEvent event){
+        if(event.sensor.getType()== Sensor.TYPE_ACCELEROMETER){
+            float[] values = event.values;
+            float x = values[0];
+            float y = values[1];
+            float z = values[2];
+
+            float accelerationSquareRoot=(x* x+ y* y+ z* z)
+                    /(SensorManager.GRAVITY_EARTH* SensorManager.GRAVITY_EARTH);
+
+            long actualTime=System.currentTimeMillis();
+
+            if(accelerationSquareRoot>=2){
+                if(actualTime- lastUpdate<200){
+                    return;
+                }
+                lastUpdate= actualTime;
+                count ++;
+                text.setText(String.valueOf(count));
+            }
+        }
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy){
+
     }
 }
