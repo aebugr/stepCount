4f5bd764e76eafdf5299e7b9add925350d42a410
Sneja <iliuvv@mail.ru>
2024-06-01 12:01:35 +0500

    Программирование окна MainActivity. Бугрина

--- app/src/main/java/com/example/stepcount/MainActivity.java
+++ app/src/main/java/com/example/stepcount/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.stepcount;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    public boolean active = true;
+    private SensorManager sensorManager;
+    private int count = 0;
+    private TextView text;
+    private long lastUpdate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.textView2);
+        text.setText(String.valueOf(count));
+        sensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
+
+
+        sensorManager.registerListener((SensorEventListener) this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+
+        lastUpdate = System.currentTimeMillis();
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        sensorManager.registerListener((SensorEventListener) this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener((SensorEventListener) this);
+    }
+    public void OnStoped(View view) {
+        active = !active;
+        if(!active) {
+            Button button = findViewById(R.id.button);
+            button.setText("ВОЗОБНОВИТЬ");
+        } else {
+            Button button = findViewById(R.id.button);
+            button.setText("ПАУЗА");
+        }
     }
 }
